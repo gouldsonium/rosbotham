53755e38d2965bda053e279e81b013f946886a3d
Stefan Gouldson <stefangouldson@gmail.com>
2024-02-29 21:33:24 +0000

    fixed SEO

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -5,5 +5,19 @@
 </template>
    
 <script setup>
-  defineProps({ blok: Object })
+  const props = defineProps({ blok: Object });
+
+  useSeoMeta({
+    title: () => props.blok?.meta_title || 'Rosbotham Finance',
+    ogTitle: () => props.blok?.meta_title || 'Rosbotham Finance',
+    description: () =>  props.blok?.meta_description || "Great financial planning is not about fancy investment or hot tips. It's about who you are, what sort of life you live and what you want out of life",
+    ogDescription: () =>  props.blok?.meta_description || "Great financial planning is not about fancy investment or hot tips. It's about who you are, what sort of life you live and what you want out of life",
+  });
+
+  useServerSeoMeta({
+    title: () => props.blok?.meta_title || 'Rosbotham Finance',
+    ogTitle: () => props.blok?.meta_title || 'Rosbotham Finance',
+    description: () => props.blok?.meta_description || "Great financial planning is not about fancy investment or hot tips. It's about who you are, what sort of life you live and what you want out of life",
+    ogDescription: () =>  props.blok?.meta_description || "Great financial planning is not about fancy investment or hot tips. It's about who you are, what sort of life you live and what you want out of life",
+  });
 </script>
